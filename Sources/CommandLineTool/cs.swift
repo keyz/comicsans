8a18f05ee2cbf6dbd17e0a7d7e5074791f88d06e
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-08-28 22:31:02 -0700

    0.3.0

--- Sources/CommandLineTool/cs.swift
+++ Sources/CommandLineTool/cs.swift
@@ -18,7 +18,7 @@ struct cs: ParsableCommand {
         discussion: """
         Converts text to pink comic sans slack emoji. https://github.com/keyz/comicsans
         """,
-        version: "0.2.0",
+        version: "0.3.0",
         helpNames: .long
     )
 

216100fd06b25071be883ccb6fffd11b1b194306
Keyan Zhang <2268452+keyz@users.noreply.github.com>
2024-09-01 23:14:09 -0700

    put spacers on both ends

--- Sources/ComicSans/ComicSansView.swift
+++ Sources/ComicSans/ComicSansView.swift
@@ -25,13 +25,13 @@ public struct ComicSansView: View {
     }
 
     public var body: some View {
-        VStack {
-            if verticalAlignment == .bottom {
+        VStack(spacing: 0) {
+            if verticalAlignment != .top {
                 if debug, #available(macOS 14.0, *) {
-                    Spacer()
+                    Spacer(minLength: 0)
                         .background(Color.yellow.opacity(0.9).containerRelativeFrame(.horizontal))
                 } else {
-                    Spacer()
+                    Spacer(minLength: 0)
                 }
             }
 
@@ -44,12 +44,12 @@ public struct ComicSansView: View {
                 .border(Color.blue.opacity(0.9), width: debug ? 1 : 0)
                 .padding(CGFloat(padding)) // NOTE: use letter "j" to test the horizontal spacing
 
-            if verticalAlignment == .top {
+            if verticalAlignment != .bottom {
                 if debug, #available(macOS 14.0, *) {
-                    Spacer()
+                    Spacer(minLength: 0)
                         .background(Color.yellow.opacity(0.9).containerRelativeFrame(.horizontal))
                 } else {
-                    Spacer()
+                    Spacer(minLength: 0)
                 }
             }
         }
@@ -142,6 +142,16 @@ public struct ComicSansView: View {
 
     #Preview("Default") {
         ContainerView(text: "Write something here and click generate")
+    }
+
+    #Preview("Zero-height spacer") {
+        ContainerView(
+            text: "Write something here and click generate. This will be longer than usual. I need a few more lines of text. And a bit more. How about now? One more line? And one more?",
+            padding: 4,
+            horizontalAlignment: .leading,
+            verticalAlignment: .center,
+            debug: true
+        )
     }
 
     #Preview("\"lfg\"") {
